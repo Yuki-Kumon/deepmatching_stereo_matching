936214df8958f03494d76bbea9905c4d291e5ede
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-09-17 16:15:05 +0900

    complete aggregation part

--- misc/Correlation_map.py
+++ misc/Correlation_map.py
@@ -35,20 +35,21 @@ class Correlation_map():
     deepmatchingみたいにピラミッド状の特徴マップを作成する
     '''
 
-    def __init__(self, img, template, window_size=3, co_map_height=5, feature_name='cv2.TM_CCOEFF_NORMED'):
+    def __init__(self, img, template, window_size=3, feature_name='cv2.TM_CCOEFF_NORMED'):
         if img.shape != template.shape:
             print('use same size images!(サイズが違うと悲しい気持ちになるので(そのうち対応したいですね))')
             sys.exit()
         self.img = img
         self.template = template
         self.window_size = window_size
-        self.co_map_height = co_map_height
 
         self.exclusive_pix = int((window_size - 1) / 2)
-        self.co_map_exclusive = int((co_map_height - 1) / 2)
         self.image_size = [x for x in img.shape]
 
         self.Feature = Feature_value(feature_name=feature_name)
+
+        self.Maxpool = Maxpool()
+        self.Maxpool.eval()
 
     def _create_atomic_patch(self):
         '''
@@ -105,25 +106,48 @@ class Correlation_map():
         for i in range(self.exclusive_pix, self.image_size[0] - self.exclusive_pix):
             for j in range(self.exclusive_pix, self.image_size[1] - self.exclusive_pix):
                 co_here = self.Feature(
-                    self.atomic_patch[i - self.exclusive_pix - self.co_map_exclusive, j - self.exclusive_pix],
+                    self.atomic_patch[i - self.exclusive_pix, j - self.exclusive_pix],
                     self.template
                 )
                 co_map[i - self.exclusive_pix, j - self.exclusive_pix] = co_here
         self.co_map = co_map
 
-    def _aggregation(self, p_window_size=3, stride=2):
+    def _aggregation(self, map):
         '''
         aggregation to make upper class co_map
         pooling window:3
         stride:1
         '''
-        co_map_list = []
-        co_map_list.append(self.co_map)
+        map_len_1 = int((map.shape[2]) / 2)
+        map_len_2 = int((map.shape[3]) / 2)
+        res = np.empty((map.shape[0], map.shape[1], map_len_1, map_len_2))
+        # max pool
+        for i in range(map.shape[0]):
+            for j in range(map.shape[1]):
+                res[i, j] = self.Maxpool(torch.from_numpy(map[i, j][None])).numpy()[0]
+        # print(res.shape)
 
-        # 原著の手順に従ってmulti-level correlation pyramidを計算する
-        N = 4
-        while N < np.max(self.co_map.shape[:2]):
-            hoge
+        # shift and average
+        output = np.empty((map_len_1, map_len_2, map_len_1, map_len_2))
+        for i in range(map_len_1):
+            for j in range(map_len_2):
+                # 平均を取る対象のインデックスを計算しておく
+                upper_left = [i * 2, j * 2]
+                upper_right = [i * 2, j * 2 + 1]
+                lower_left = [i * 2 + 1, j * 2]
+                lower_right = [i * 2 + 1, j * 2 + 1]
+                # shift
+                upper_left_img = np.zeros(res.shape[2:])
+                upper_left_img[1:, 1:] = res[upper_left[0], upper_left[1]][:-1, :-1]
+                upper_right_img = np.zeros(res.shape[2:])
+                upper_right_img[1:, :-1] = res[upper_right[0], upper_right[1]][:-1, 1:]
+                lower_left_img = np.zeros(res.shape[2:])
+                lower_left_img[:-1, 1:] = res[lower_left[0], lower_left[1]][1:, :-1]
+                lower_right_img = np.zeros(res.shape[2:])
+                lower_right_img[:-1, :-1] = res[lower_right[0], lower_right[1]][1:, 1:]
+                # average
+                output[i, j] = (upper_left_img + upper_right_img + lower_left_img + lower_right_img) / 4
+        return output
 
 
 class Maxpool(nn.Module):
@@ -151,6 +175,7 @@ if __name__ == '__main__':
     print(np.max(hoge.shape[2:]))
     """
 
+    """
     # img = torch.from_numpy(np.random.rand(1, 1, 32, 32))
     Mod = Maxpool()
     # print(Mod(img).size())
@@ -163,17 +188,35 @@ if __name__ == '__main__':
     for i in range(map.shape[0]):
         for j in range(map.shape[1]):
             res[i, j] = Mod(torch.from_numpy(map[i, j][None])).numpy()[0]
+    # print(res.shape)
+
     # shift and average
+    output = np.empty((map_len_1, map_len_2, map_len_1, map_len_2))
     for i in range(map_len_1):
-        for j in raneg(map_len_2):
-            upper_left
-    print(res)
-
-
+        for j in range(map_len_2):
+            # 平均を取る対象のインデックスを計算しておく
+            upper_left = [i * 2, j * 2]
+            upper_right = [i * 2, j * 2 + 1]
+            lower_left = [i * 2 + 1, j * 2]
+            lower_right = [i * 2 + 1, j * 2 + 1]
+            # shift
+            upper_left_img = np.zeros(res.shape[2:])
+            upper_left_img[1:, 1:] = res[upper_left[0], upper_left[1]][:-1, :-1]
+            upper_right_img = np.zeros(res.shape[2:])
+            upper_right_img[1:, :-1] = res[upper_right[0], upper_right[1]][:-1, 1:]
+            lower_left_img = np.zeros(res.shape[2:])
+            lower_left_img[:-1, 1:] = res[lower_left[0], lower_left[1]][1:, :-1]
+            lower_right_img = np.zeros(res.shape[2:])
+            lower_right_img[:-1, :-1] = res[lower_right[0], lower_right[1]][1:, 1:]
+            # average
+            output[i, j] = (upper_left_img + upper_right_img + lower_left_img + lower_right_img) / 4
+    # print(output)
     """
+
     cls = Correlation_map(img1, img2)
     cls._create_atomic_patch()
     # cls._create_initial_co_map()
     cls._create_simple_initial_co_map()
-    print(cls.co_map.shape)
-    """
+    res = cls._aggregation(cls.co_map)
+    print(res.shape)
+    # print(cls.co_map.shape)
