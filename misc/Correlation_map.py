ad428cb3ded0ef95266e72837b16a4d9135eacbe
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-09-17 15:57:17 +0900

    add some lines

--- misc/Correlation_map.py
+++ misc/Correlation_map.py
@@ -14,6 +14,10 @@ import sys
 import numpy as np
 import cv2
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 try:
     from misc.Feature_value import Feature_value
     print('misc.Feature_value loaded')
@@ -64,6 +68,7 @@ class Correlation_map():
         # データ型を符号なし整数に変えておく
         self.atomic_patch = atomic_patch.astype(np.uint8)
 
+    """
     def _create_initial_co_map(self):
         '''
         初めの相関マップを計算する。
@@ -84,23 +89,91 @@ class Correlation_map():
                 )
                 co_map[i - self.exclusive_pix, j - self.exclusive_pix] = co_here
         self.co_map = co_map
+    """
 
-    def _aggregation(self):
+    def _create_simple_initial_co_map(self):
+        '''
+        初めの相関マップを計算する。
+        普通のdeepmatching用
+        '''
+        co_map = np.empty((
+            self.atomic_patch.shape[0],
+            self.atomic_patch.shape[1],
+            self.atomic_patch.shape[0],
+            self.atomic_patch.shape[1]
+        ))
+        for i in range(self.exclusive_pix, self.image_size[0] - self.exclusive_pix):
+            for j in range(self.exclusive_pix, self.image_size[1] - self.exclusive_pix):
+                co_here = self.Feature(
+                    self.atomic_patch[i - self.exclusive_pix - self.co_map_exclusive, j - self.exclusive_pix],
+                    self.template
+                )
+                co_map[i - self.exclusive_pix, j - self.exclusive_pix] = co_here
+        self.co_map = co_map
+
+    def _aggregation(self, p_window_size=3, stride=2):
         '''
         aggregation to make upper class co_map
+        pooling window:3
+        stride:1
         '''
-        pass
+        co_map_list = []
+        co_map_list.append(self.co_map)
+
+        # 原著の手順に従ってmulti-level correlation pyramidを計算する
+        N = 4
+        while N < np.max(self.co_map.shape[:2]):
+            hoge
+
+
+class Maxpool(nn.Module):
+
+    def __init__(self, window=3, stride=2, padding=1):
+        super(Maxpool, self).__init__()
+
+        self.pool = nn.MaxPool2d(window, stride, padding=padding)
+
+    def forward(self, x):
+        return self.pool(x)
 
 
 if __name__ == '__main__':
     """
     sanity check
-    run on deepmatching_stereo_matching dir
     """
 
-    img1 = cv2.imread('./data/band3s.tif', cv2.IMREAD_GRAYSCALE)[:500, :500]
-    img2 = cv2.imread('./data/band3bs.tif', cv2.IMREAD_GRAYSCALE)[:500, :500]
+    img1 = cv2.imread('./data/band3s.tif', cv2.IMREAD_GRAYSCALE)[:100, :100]
+    img2 = cv2.imread('./data/band3bs.tif', cv2.IMREAD_GRAYSCALE)[:100, :100]
 
+    """
+    hoge = np.random.rand(1, 2, 3, 4)
+
+    print(np.max(hoge.shape[2:]))
+    """
+
+    # img = torch.from_numpy(np.random.rand(1, 1, 32, 32))
+    Mod = Maxpool()
+    # print(Mod(img).size())
+
+    map = np.random.rand(98, 98, 98, 98)
+    map_len_1 = int((map.shape[2]) / 2)
+    map_len_2 = int((map.shape[3]) / 2)
+    res = np.empty((map.shape[0], map.shape[1], map_len_1, map_len_2))
+    # max pool
+    for i in range(map.shape[0]):
+        for j in range(map.shape[1]):
+            res[i, j] = Mod(torch.from_numpy(map[i, j][None])).numpy()[0]
+    # shift and average
+    for i in range(map_len_1):
+        for j in raneg(map_len_2):
+            upper_left
+    print(res)
+
+
+    """
     cls = Correlation_map(img1, img2)
     cls._create_atomic_patch()
-    cls._create_initial_co_map()
+    # cls._create_initial_co_map()
+    cls._create_simple_initial_co_map()
+    print(cls.co_map.shape)
+    """
