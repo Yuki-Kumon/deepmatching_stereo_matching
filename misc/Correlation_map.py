66f9adb1dd8c38cc80f427fe949879f611a04bc8
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-09-30 16:27:20 +0900

    update joblib part

--- misc/Correlation_map.py
+++ misc/Correlation_map.py
@@ -18,6 +18,8 @@ import torch
 import torch.nn as nn
 # import torch.nn.functional as F
 
+from joblib import Parallel, delayed
+
 try:
     from misc.Feature_value import Feature_value
     print('misc.Feature_value loaded')
@@ -90,6 +92,7 @@ class Correlation_map():
                 co_map[i - self.exclusive_pix, j - self.exclusive_pix] = co_here
         self.co_map = co_map
 
+    """
     def _aggregation(self, map):
         '''
         aggregation to make upper class co_map
@@ -115,17 +118,7 @@ class Correlation_map():
                 upper_right = [i * 2, j * 2 + 1]
                 lower_left = [i * 2 + 1, j * 2]
                 lower_right = [i * 2 + 1, j * 2 + 1]
-                # shift
-                """
-                upper_left_img = np.zeros(res.shape[2:])
-                upper_left_img[1:, 1:] = res[upper_left[0], upper_left[1]][:-1, :-1]
-                upper_right_img = np.zeros(res.shape[2:])
-                upper_right_img[1:, :-1] = res[upper_right[0], upper_right[1]][:-1, 1:]
-                lower_left_img = np.zeros(res.shape[2:])
-                lower_left_img[:-1, 1:] = res[lower_left[0], lower_left[1]][1:, :-1]
-                lower_right_img = np.zeros(res.shape[2:])
-                lower_right_img[:-1, :-1] = res[lower_right[0], lower_right[1]][1:, 1:]
-                """
+
                 # shiftはpaddingしているので不要
                 upper_left_img = res[upper_left[0], upper_left[1]]
                 upper_right_img = res[upper_right[0], upper_right[1]]
@@ -133,6 +126,50 @@ class Correlation_map():
                 lower_right_img = res[lower_right[0], lower_right[1]]
                 # average
                 output[i, j] = (upper_left_img + upper_right_img + lower_left_img + lower_right_img) / 4
+        return output
+    """
+
+    def _aggregation(self, map):
+        '''
+        aggregation to make upper class co_map
+        pooling window:3
+        stride:2
+        この設定は変えない(なぜならつらいから。。。)
+        '''
+        map_len_1 = int((map.shape[2]) / 2)
+        map_len_2 = int((map.shape[3]) / 2)
+        res = np.empty((map.shape[0], map.shape[1], map_len_1, map_len_2))
+
+        # max pool
+        for i in range(map.shape[0]):
+            for j in range(map.shape[1]):
+                res[i, j] = self.Maxpool(torch.from_numpy(map[i, j][None])).numpy()[0]
+
+        # shift and average
+        output = np.empty((map_len_1, map_len_2, map_len_1, map_len_2))
+
+        # process
+        def process(i, j, res):
+            # 平均を取る対象のインデックスを計算しておく
+            upper_left = [i * 2, j * 2]
+            upper_right = [i * 2, j * 2 + 1]
+            lower_left = [i * 2 + 1, j * 2]
+            lower_right = [i * 2 + 1, j * 2 + 1]
+
+            # shiftはpaddingしているので不要
+            upper_left_img = res[upper_left[0], upper_left[1]]
+            upper_right_img = res[upper_right[0], upper_right[1]]
+            lower_left_img = res[lower_left[0], lower_left[1]]
+            lower_right_img = res[lower_right[0], lower_right[1]]
+            # average
+            return (upper_left_img + upper_right_img + lower_left_img + lower_right_img) / 4, (i, j)
+
+        # parallel processing
+        response = Parallel(n_jobs=-1)([delayed(process)(i, j, res) for i in range(map_len_1) for j in range(map_len_2)])
+        # assign
+        for res_here in response:
+            output[res_here[1]] = res_here[0]
+
         return output
 
     def _multi_level_correlation_pyramid(self):
