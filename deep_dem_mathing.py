162e5494b2fbfa9bd872b7da4c9f2718538ad148
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-10-01 15:13:05 +0900

    add a file

---
+++ deep_dem_mathing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+stereo mathing with deepmatching
+Author :
+    Yuki Kumon
+Last Update :
+    2019-10-01
+"""
+
+from misc.Correlation_map import Correlation_map
+from misc.Matching import Matching
+from misc.Calc_difference import Calc_difference
+
+import numpy as np
+import cv2
