3d43076380727aa2a1deb7927c915ff81817c97a
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-10-01 15:09:34 +0900

    update feature value to compare new and old method.

--- misc/Feature_value.py
+++ misc/Feature_value.py
@@ -12,6 +12,7 @@ Last Update :
 import sys
 
 import cv2
+import numpy as np
 
 
 class Feature_value():
@@ -20,7 +21,7 @@ class Feature_value():
     '''
 
     def __init__(self, feature_name='cv2.TM_CCOEFF_NORMED'):
-        FEATURE_NAME_LIST = ['cv2.TM_CCOEFF_NORMED']
+        FEATURE_NAME_LIST = ['cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR_NORMED']  # cv2.TM_CCORR_NORMEDは既存手法との比較用(ただ畳み込みをおこなうのみの類似度)
 
         if feature_name not in FEATURE_NAME_LIST:
             print('invalid feature_name \'{}\' is inputed!'.format(feature_name))
@@ -30,4 +31,9 @@ class Feature_value():
 
     def __call__(self, img, template):
         res = (cv2.matchTemplate(img, template, self.method) + 1.0) / 2.0
+        """
+        res = cv2.matchTemplate(img, template, self.method)
+        res = (res - np.mean(res)) / np.std(res)
+        res = (res + 1.0) / 2.0
+        """
         return res
