2837f85d163c08f85fcd424953417cc5df1858d6
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-12-12 20:16:16 +0900

    add some lines

--- misc/Correlation_map_v2.py
+++ misc/Correlation_map_v2.py
@@ -5,7 +5,7 @@ correlation map version 2
 Author :
     Yuki Kumon
 Last Update :
-    2019-12-05
+    2019-12-12
 """
 
 
@@ -28,10 +28,10 @@ class CorrelationMapV2():
     deepmatchingみたいにピラミッド状の特徴マップを作成する
     '''
 
-    def __init__(self, img, template, window_size=3, search_window_size=[9, 9], r_lam=1.4, feature_name='cv2.TM_CCOEFF_NORMED'):
+    def __init__(self, img, template, window_size=3, search_window_size=[16, 16], r_lam=1.4, feature_name='cv2.TM_CCOEFF_NORMED'):
         '''
         各window_sizeは奇数
-        search_window_sizeはリストで指定(今の所正方形のみ)
+        search_window_sizeはリストで指定(今の所正方形のみ、2のn乗のみ)
         '''
         if img.shape != template.shape:
             print('use same size images!(リサイズすると意味がないので)')
@@ -42,17 +42,15 @@ class CorrelationMapV2():
         self.search_window_size = search_window_size
         self.lam = r_lam
 
-        self.exclusive_pix = int((window_size - 1) / 2)
+        self.exclusive_pix = int((window_size) / 2)
         # cv2 matchi templateではパディングを行わないのでここで大きめに取っておく
-        self.search_range = [int((search_window_size[i] - 1) / 2) + self.exclusive_pix for i in range(2)]
-        self.template_padding_pix = [int((search_window_size[i] - 1) / 2) for i in range(2)]
+        self.search_range = [int((search_window_size[i]) / 2) + self.exclusive_pix for i in range(2)]
+        self.template_padding_pix = [int((search_window_size[i]) / 2) for i in range(2)]
         self.image_size = [x for x in img.shape]
 
         self.Feature = Feature_value(feature_name=feature_name)
         self.Maxpool = Maxpool()
         self.Maxpool.eval()
-        self.Maxpool_p = Maxpool_padding()
-        self.Maxpool_p.eval()
 
     def _create_atomic_patch(self):
         '''
@@ -90,9 +88,9 @@ class CorrelationMapV2():
                     self.atomic_patch[i - self.exclusive_pix, j - self.exclusive_pix],  # 元画像
                     self.template[
                         self.template_padding_pix[0] + i - self.search_range[0]:
-                        self.template_padding_pix[0] + i + self.search_range[0] + 1,
+                        self.template_padding_pix[0] + i + self.search_range[0],
                         self.template_padding_pix[1] + j - self.search_range[1]:
-                        self.template_padding_pix[1] + j + self.search_range[1] + 1
+                        self.template_padding_pix[1] + j + self.search_range[1]
                     ]  # テンプレート画像(近傍)
                 )
                 co_map[i - self.exclusive_pix, j - self.exclusive_pix] = co_here
@@ -112,16 +110,10 @@ class CorrelationMapV2():
         res = np.empty((map.shape[0], map.shape[1], map_len_1, map_len_2))
 
         # max pool
-        if (map_len_1 % 2 == 0):
-            for i in range(map.shape[0]):
-                for j in range(map.shape[1]):
-                    # 各画素に対応する特徴マップに対してmax poolingする
-                    res[i, j] = self.Maxpool(torch.from_numpy(map[i, j][None])).numpy()[0]
-        else:
-            for i in range(map.shape[0]):
-                for j in range(map.shape[1]):
-                    # 各画素に対応する特徴マップに対してmax poolingする
-                    res[i, j] = self.Maxpool_p(torch.from_numpy(map[i, j][None])).numpy()[0]
+        for i in range(map.shape[0]):
+            for j in range(map.shape[1]):
+                # 各画素に対応する特徴マップに対してmax poolingする
+                res[i, j] = self.Maxpool(torch.from_numpy(map[i, j][None])).numpy()[0]
 
         # shift and average
         output = np.empty((map_im_len_1, map_im_len_2, map_len_1, map_len_2))
@@ -167,21 +159,54 @@ class CorrelationMapV2():
         ] = temp
         self.template = self.template.astype(np.uint8)
 
+    def _multi_level_correlation_pyramid(self):
+        '''
+        aggregationを繰り返し、multi-level correlation pyramidを計算する
+        初めの特徴マップはFeatureに依存(現時点では原著と異なりznccを使っている)
+        '''
 
-class Maxpool_padding(nn.Module):
+        # 原著の手順に従ってmulti-level correlation pyramidを計算する
+        co_map_list = []
+        co_map = self.co_map
+        co_map = self._rectification(co_map)
+        co_map_list.append(co_map)
+        N = 1
+        iteration = 1
+        while N < np.min(self.co_map.shape[2:]):
+            # aggregation
+            aggregated_map = self._aggregation(co_map)
+            aggregated_map = self._rectification(aggregated_map)
+            co_map_list.append(aggregated_map)
+            del co_map
+            co_map = aggregated_map
+            N *= 2
+            iteration += 1
+        self.co_map_list = co_map_list
+        self.iteration = iteration
+        self.N_map = N
 
-    def __init__(self, window=3, stride=2, padding=1):
-        super(Maxpool_padding, self).__init__()
+    def _rectification(self, map):
+        return map**self.lam
 
-        self.pool = nn.MaxPool2d(window, stride, padding=padding)
+    def __call__(self):
+        '''
+        特徴マップの計算までを行う
+        '''
+        self._template_padding()
+        self._create_atomic_patch()
+        print('complete to create atomic patch')
+        self._create_initial_co_map()
+        print('complete to create initial correlation map')
+        self._multi_level_correlation_pyramid()
+        print('complete to create multi-level correlation pyramid')
+        print('pyramid level: {}, N={}'.format(self.iteration, self.N_map))
 
-    def forward(self, x):
-        return self.pool(x)
+        return self.co_map_list
 
 
 class Maxpool(nn.Module):
 
-    def __init__(self, window=3, stride=2, padding=0):
+    def __init__(self, window=3, stride=2, padding=1):
         super(Maxpool, self).__init__()
 
         self.pool = nn.MaxPool2d(window, stride, padding=padding)
@@ -200,13 +225,18 @@ if __name__ == '__main__':
 
     cls = CorrelationMapV2(img1, img2)
 
+    """
     cls._template_padding()
     cls._create_atomic_patch()
     cls._create_initial_co_map()
+    """
+    """
     print(cls.co_map.shape)
     out = cls._aggregation(cls.co_map)
     print(out.shape)
     out = cls._aggregation(out)
     print(out.shape)
     out = cls._aggregation(out)
-    print(out)
+    print(out.shape)
+    """
+    cls()
---
+++ misc/Matching_v2.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+"""
+matching on correlation map
+Author :
+    Yuki Kumon
+Last Update :
+    2019-12-12
+"""
+
+
+import sys
+
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class Matching():
+    '''
+    multi-level correlation pyramidからマッチングを行う
+    原著の14式に従って計算していく
+    '''
+
+    def __init__(self, Co_obj=None):
+        try:
+            Co_obj.co_map_list
+        except AttributeError as e:
+            print('Error!: {}'.format(e))
+            print('please run \'obj=Correlation_map()\' and \'obj()\' first.')
+            sys.exit()
+
+        self.obj = Co_obj
+        self.Padding = Zero_padding()
+        self.Padding.eval()
+
+    def _calc_near_match(self, co_map, p, p_dot):
+        '''
+        pとp_dotはco_mapの解像度に合わせた座標
+        p_iとして共に少し4つのchildrenとして移動させた上でこの関数に読ませることにする
+        原著の13式に従って対応点を計算する
+        13式のmを計算する
+        '''
+        # pに対応した特徴マップを取り出す
+        map_on_p = co_map[p[0], p[1]]
+        # 端対策にゼロパディングする
+        map_on_p_padded = self.Padding(torch.from_numpy(map_on_p[None])).numpy()[0]
+        # p_dot周辺の3×3を取り出す(座標のずれに注意)
+        co_map_near_p_dot = map_on_p_padded[p_dot[0] - p[0] - 1 + 1:p_dot[0] - p[0] + 2 + 1, p_dot[1] - p[1] - 1 + 1:p_dot[1] - p[1] + 2 + 1]
+        # この中で相関値最大の座標を計算
+        m = np.unravel_index(np.argmax(co_map_near_p_dot), co_map_near_p_dot.shape)
+        # 全部0だった場合の処理(もし最大値がだいたいゼロなら、移動していないとみなす)
+        if np.max(co_map_near_p_dot) < 0.0001:
+            m = [1, 1]
+        # p_dot, 新たな相関値を返す
+        # print(p_dot[0] - 1 + 1, p_dot[0] + 2 + 1, p_dot[1] - 1 + 1, p_dot[1] + 2 + 1, co_map_near_p_dot, co_map_near_p_dot)
+        return p_dot[0] + m[0] - 1, p_dot[1] + m[1] - 1, co_map_near_p_dot[m[0], m[1]] + map_on_p[p_dot[0], p_dot[1]]
+
+    def _initial_move_map(self):
+        '''
+        ピラミッドの頂点での動きのマップを計算する
+        '''
+        # 各ピクセルに(対応するy, 対応するx, 相関値s)が格納されている
+        map = np.zeros((3, self.obj.co_map_list[-1].shape[0], self.obj.co_map_list[-1].shape[1]))
+        for i in range(map.shape[1]):
+            for j in range(map.shape[2]):
+                map[:2, i, j] = i, j
+                # 初めの移動量を計算(これは必要なさそうではある)
+                map[:, i, j] = self._calc_near_match(self.obj.co_map_list[-1], (i, j), map[:2, i, j].astype('int64'))
+        self.map = map
+        self.map_idx = -1
+        self.N = self.obj.N_map
+
+    def _B(self):
+        '''
+        原著の14式
+        各パッチの左上の座標を用いて計算する
+        '''
+        # Nとiterarionとmapはこの後更新する。N > 0でwhileループで回せばいいと思う
+        # N = self.obj.N_map
+        N = self.N
+        map_idx = self.map_idx - 1
+        map_here = self.map
+        map_updated = np.empty((3, map_here.shape[1] * 2, map_here.shape[2] * 2))
+
+        # 式中のベクトルo
+        o_list = np.array([[1, 1], [0, 1], [1, 0], [0, 0]])
+
+        for i in range(map_here.shape[1]):
+            for j in range(map_here.shape[2]):
+                # 対応するquadrantの一つ上での解像度での座標を計算しておく
+                p_upper_left = np.array([i * 2, j * 2])
+                p_dot_upper_left = (map_here[:2, i, j] * 2).astype('int64')
+                # ここでの相関値を取り出しておく
+                # s_here = self.map[2, i, j]
+                # qauddrantごとに14式の計算を行い、mapを更新する
+                for o_idx in range(4):
+                    o_here = o_list[o_idx]
+                    p_here = p_upper_left + o_here
+                    p_dot_here = p_dot_upper_left + o_here
+                    # 13式に従い、mを計算、mapを更新する
+                    map_updated[:, p_here[0], p_here[1]] = self._calc_near_match(
+                        self.obj.co_map_list[map_idx],
+                        (p_here[0], p_here[1]),
+                        (p_dot_here[0], p_dot_here[1])
+                    )
+                    # print(map_updated[:, p_here[0], p_here[1]])
+        # 諸々の値を更新
+        self.map_idx -= 1
+        self.N = int(N / 2)
+        del self.map
+        self.map = map_updated
+
+    def _calc_match(self):
+        '''
+        原著のB式を繰り返し用いてマッチングを計算する
+        '''
+        # N > 0であれば計算を行う
+        while 1:
+            self._B()
+            if self.N == 1:
+                break
+
+    def __call__(self):
+        '''
+        multi-level correlation pyramidからマッチング計算を行う
+        '''
+        self._initial_move_map()
+        print('complete to create initial matching map')
+        self._calc_match()
+        print('complete backtracking')
+
+        return self.map
+
+
+class Zero_padding(nn.Module):
+    '''
+    原著の14式の計算のため、ゼロパディングしておく
+    '''
+
+    def __init__(self):
+        super(Zero_padding, self).__init__()
+        self.m = nn.ZeroPad2d(1)
+
+    def forward(self, x):
+        return self.m(x)
+
+
+if __name__ == '__main__':
+    """
+    sanity check
+    """
+    from Correlation_map_v2 import CorrelationMapV2
+
+    # atomicな特徴マップが一辺が2^nじゃないとバグるカス実装です。。。
+    img1 = cv2.imread('./data/band3s.tif', cv2.IMREAD_GRAYSCALE)[500:500 + 130, 500:500 + 130]
+    img2 = cv2.imread('./data/band3bs.tif', cv2.IMREAD_GRAYSCALE)[500:500 + 130, 500:500 + 130]
+    print(img1.shape)
+
+    co_cls = CorrelationMapV2(img1, img2)
+    co_cls()
+
+    # 試しに書き出し
+    """
+    cv2.imwrite('out.png', co_cls.co_map_list[1][0, 0] * 50)
+    cv2.imwrite('out0.png', co_cls.co_map_list[0][0, 0] * 50)
+    cv2.imwrite('here.png', img1)
+    """
+
+    cls = Matching(co_cls)
+    # cls._initial_move_map()
+    # cls._B()
+    # cls._calc_match()
+    out = cls()
